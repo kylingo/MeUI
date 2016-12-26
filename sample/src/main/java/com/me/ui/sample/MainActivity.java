46a14f3a74fa53f49a2ca4c36c6e102270c14b04
七哥 <studiotang.cn@gmail.com>
2016-12-26 17:11:59 +0800

    Add pull to refresh translation

--- sample/src/main/java/com/me/ui/sample/MainActivity.java
+++ sample/src/main/java/com/me/ui/sample/MainActivity.java
@@ -1,45 +1,74 @@
 package com.me.ui.sample;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.me.ui.refresh.PullToRefreshBase;
 import com.me.ui.refresh.PullToRefreshListView;
 
 import java.util.ArrayList;
 import java.util.List;
 
+public class MainActivity extends AppCompatActivity implements PullToRefreshBase.OnRefreshListener2 {
 
-public class MainActivity extends AppCompatActivity {
+    private static final int DELAY_REFRESH = 1000;
 
     private PullToRefreshListView mPullToRefreshListView;
-    private ListView mListView;
+    private ArrayAdapter<String> mArrayAdapter;
+    private Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initData();
         initView();
+    }
+
+    private void initData() {
+        mHandler = new Handler();
     }
 
     private void initView() {
         mPullToRefreshListView = (PullToRefreshListView) findViewById(R.id.lv_main);
-        assert mPullToRefreshListView != null;
-        mListView = mPullToRefreshListView.getRefreshableView();
+        mPullToRefreshListView.setOnRefreshListener(this);
+        ListView mListView = mPullToRefreshListView.getRefreshableView();
 
-        mListView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, getData()));
+        mArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, getData());
+        mListView.setAdapter(mArrayAdapter);
     }
 
     private List<String> getData() {
         List<String> dataList = new ArrayList<>();
         int listSize = 20;
 
-        for (int i = 0; i < listSize; i++) {
+        int offset = mArrayAdapter != null ? mArrayAdapter.getCount() : 0;
+        for (int i = offset; i < listSize + offset; i++) {
             dataList.add("item" + i);
         }
 
         return dataList;
+    }
+
+    @Override
+    public void onPullDownToRefresh(PullToRefreshBase refreshView) {
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mArrayAdapter.addAll(getData());
+                mArrayAdapter.notifyDataSetChanged();
+
+                mPullToRefreshListView.onRefreshComplete();
+            }
+        }, DELAY_REFRESH);
+    }
+
+    @Override
+    public void onPullUpToRefresh(PullToRefreshBase refreshView) {
+
     }
 }
