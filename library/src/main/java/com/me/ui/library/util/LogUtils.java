94839b6a680af6d10b93333eadb9d0f7dd69286e
kylingo <studiotang.cn@gmail.com>
2017-06-26 12:40:20 +0800

    Add log module

---
+++ library/src/main/java/com/me/ui/library/util/LogUtils.java
@@ -0,0 +1,43 @@
+package com.me.ui.library.util;
+
+import android.util.Log;
+
+/**
+ * @author tangqi on 17-6-26.
+ */
+public class LogUtils {
+
+    // log tag by class name
+    public static void d(Class clazz, String msg) {
+        Log.d(clazz.getSimpleName(), msg);
+    }
+
+    public static void i(Class clazz, String msg) {
+        Log.i(clazz.getSimpleName(), msg);
+    }
+
+    public static void w(Class clazz, String msg) {
+        Log.w(clazz.getSimpleName(), msg);
+    }
+
+    public static void e(Class clazz, String msg) {
+        Log.e(clazz.getSimpleName(), msg);
+    }
+
+    // log tag by string
+    public static void d(String tag, String msg) {
+        Log.d(tag, msg);
+    }
+
+    public static void i(String tag, String msg) {
+        Log.d(tag, msg);
+    }
+
+    public static void w(String tag, String msg) {
+        Log.d(tag, msg);
+    }
+
+    public static void e(String tag, String msg) {
+        Log.d(tag, msg);
+    }
+}
--- library/src/main/java/com/me/ui/library/util/Test.java
+++
@@ -1,7 +0,0 @@
-package com.me.ui.library.util;
-
-/**
- * @author tangqi on 17-6-20.
- */
-public class Test {
-}
