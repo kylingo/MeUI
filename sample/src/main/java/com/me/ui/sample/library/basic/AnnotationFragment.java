5ccb5f36300ed833c36b87c3b453a42d9a03c92d
kylingo <studiotang.cn@gmail.com>
2018-07-06 02:13:45 +0800

    Add annotation sample

--- sample/src/main/java/com/me/ui/sample/library/basic/AnnotationFragment.java
+++ sample/src/main/java/com/me/ui/sample/library/basic/AnnotationFragment.java
@@ -1,7 +1,13 @@
 package com.me.ui.sample.library.basic;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.kylingo.annotation.BindView;
+import com.kylingo.annotation.OnClick;
+import com.kylingo.annotation.ViewFinder;
 import com.me.ui.sample.R;
 import com.me.ui.sample.base.BaseFragment;
 
@@ -10,6 +16,12 @@ import com.me.ui.sample.base.BaseFragment;
  */
 public class AnnotationFragment extends BaseFragment {
 
+    @BindView(R.id.btn_annotation)
+    Button btnAnnotation;
+
+    @BindView(R.id.tv_annotation)
+    TextView tvAnnotation;
+
     @Override
     protected int getContentViewId() {
         return R.layout.fragment_annotation;
@@ -17,6 +29,16 @@ public class AnnotationFragment extends BaseFragment {
 
     @Override
     protected void initView(View view) {
+        ViewFinder.inject(this, view);
+    }
 
+    @OnClick(R.id.btn_annotation)
+    public void onButtonClick() {
+        Toast.makeText(getActivity(), "onButtonClick", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.tv_annotation)
+    public void onTextClick() {
+        Toast.makeText(getActivity(), "onTextClick", Toast.LENGTH_SHORT).show();
     }
 }
