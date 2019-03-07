7b8e8ae7abaab83f05a1e53a89ba68ef96fbc2d6
kylingo <studiotang.cn@gmail.com>
2019-03-07 19:05:11 +0800

    Add material button sample

---
+++ sample/src/main/java/com/me/ui/sample/widget/basic/MaterialButtonFragment.java
@@ -0,0 +1,24 @@
+package com.me.ui.sample.widget.basic;
+
+
+import android.view.View;
+
+import com.me.ui.sample.R;
+import com.me.ui.sample.base.BaseFragment;
+
+/**
+ * @author kylingo
+ * @since 2019/03/07 18:06
+ */
+public class MaterialButtonFragment extends BaseFragment {
+
+    @Override
+    protected int getContentViewId() {
+        return R.layout.fragment_material_bottom;
+    }
+
+    @Override
+    protected void initView(View view) {
+
+    }
+}
