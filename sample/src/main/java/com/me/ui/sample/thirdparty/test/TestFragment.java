f90db665de5152715534b0608e2a57674d40298a
kylingo <studiotang.cn@gmail.com>
2019-11-21 10:09:17 +0800

    Delete scheme test

--- sample/src/main/java/com/me/ui/sample/thirdparty/test/TestFragment.java
+++ sample/src/main/java/com/me/ui/sample/thirdparty/test/TestFragment.java
@@ -1,7 +1,6 @@
 package com.me.ui.sample.thirdparty.test;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.widget.Toast;
 
 import com.me.ui.library.sample.SampleFragment;
@@ -27,8 +26,6 @@ public class TestFragment extends SampleFragment<String> {
     protected void addItems(List<String> items) {
         items.add("Toast");
         items.add("输入法");
-        items.add("Scheme");
-        items.add("Profile");
         items.add("FlagMask");
     }
 
@@ -46,18 +43,6 @@ public class TestFragment extends SampleFragment<String> {
                 break;
             }
 
-            case "Scheme": {
-                schemeTest();
-                break;
-            }
-
-            case "Profile": {
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse("imgotv://noahprofile?uid=6c7b934382814507b45df4cb8c99988d"));
-                startActivity(intent);
-                break;
-            }
-
             case "FlagMask": {
                 flagMaskTest();
                 break;
@@ -67,28 +52,6 @@ public class TestFragment extends SampleFragment<String> {
     }
 
     @TryCatch
-    private void schemeTest() {
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        // 底层页
-//        String scheme = "imgotv://minivideo?vid=1d10db2c9dc44756a59e00312ab9144d";
-
-        // 微剧
-//        String scheme = "imgotv://minivideo?vid=4c4681ab572d476c9a3df452a9eeb5cb&episodeType=1";
-
-        // h5
-//        String scheme = "mgqiezi://web?url=https://nm.mgtv.com/artist/dist/red-packet.html";
-
-        // 妄想少女MOMO
-//        String scheme = "imgotv://minivideo?vid=cbc1a6c76d4d41b8a5546e93acd7e502&episodeType=1";
-//        String scheme = "mgqiezi://player?vid=cbc1a6c76d4d41b8a5546e93acd7e502&episodeType=1";
-
-        // 大芒
-        String scheme = "mgqiezi://player?vid=acf6559506dc4869b0f74dc16a0bbeab&mangoType=1";
-
-        intent.setData(Uri.parse(scheme));
-        startActivity(intent);
-    }
-
     private void flagMaskTest() {
         for (int i = 0; i < 8; i++) {
             flagOperation(i, FLAG_1);
