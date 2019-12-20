aea34e4b83731f6c1bf04e45f98d683d5ad9605d
kylingo <studiotang.cn@gmail.com>
2019-12-20 09:44:59 +0800

    Add oom test

--- sample/src/main/java/com/me/ui/sample/library/basic/LibraryBasicFragment.java
+++ sample/src/main/java/com/me/ui/sample/library/basic/LibraryBasicFragment.java
@@ -15,9 +15,10 @@ public class LibraryBasicFragment extends SampleListFragment {
     @Override
     protected void addItems(List<FragmentBean> items) {
         items.add(new FragmentBean("注解", AnnotationFragment.class));
-        items.add(new FragmentBean("DataBinding", null));
         items.add(new FragmentBean("照相", TakePhotoFragment.class));
         items.add(new FragmentBean("升级", UpgradeFragment.class));
+        items.add(new FragmentBean("DataBinding", null));
+        items.add(new FragmentBean("OOM", null));
     }
 
     @Override
@@ -28,8 +29,28 @@ public class LibraryBasicFragment extends SampleListFragment {
                 startActivity(intent);
                 return;
             }
+
+            case "OOM": {
+                testOOM();
+                return;
+            }
         }
 
         super.onClickItem(item);
+    }
+
+    /**
+     * OOM可以捕获异常堆栈，仍然会崩溃
+     */
+    private void testOOM() {
+        try {
+            throwOutOfMemoryError();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void throwOutOfMemoryError() {
+        throw new OutOfMemoryError();
     }
 }
