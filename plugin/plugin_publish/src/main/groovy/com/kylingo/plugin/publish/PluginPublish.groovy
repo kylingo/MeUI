a196be17a32a29f31af8e7d068a4d57b0fb6e4fa
kylingo <studiotang.cn@gmail.com>
2019-12-24 10:29:40 +0800

    Add Local maven test

--- plugin/plugin_publish/src/main/groovy/com/kylingo/plugin/publish/PluginPublish.groovy
+++ plugin/plugin_publish/src/main/groovy/com/kylingo/plugin/publish/PluginPublish.groovy
@@ -7,8 +7,8 @@ public class PluginPublish implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
-        System.out.println("========================");
-        System.out.println("hello gradle plugin!");
-        System.out.println("========================");
+        System.out.println("========================")
+        System.out.println("hello gradle plugin version:" + "1.0.4")
+        System.out.println("========================")
     }
 }
